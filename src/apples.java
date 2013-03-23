fe20cde9eaa4af3f9d0e9442920329d49915d08e
Julien Bonack <bonackman911@gmail.com>
2013-03-23 18:39:12 -0500

    I am changing the spacing

--- src/apples.java
+++ src/apples.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
 
 public class apples{
-       public static void main(String  args[])
-       {
-        Scanner juju = new Scanner(System.in);
-        
-     double fnum, snum, answer;
-     System.out.println("Enter first number");
-     fnum = juju.nextDouble();
-     System.out.println("Enter second number");
-     snum = juju.nextDouble();
-     answer = fnum + snum;
-     System.out.println(answer);
-     
-       
-       
+	public static void main(String  args[])
+	{
+		Scanner juju = new Scanner(System.in);
+	        
+	     double fnum, snum, answer;
+	     System.out.println("Enter first number");
+	     fnum = juju.nextDouble();
+	     System.out.println("Enter second number");
+	     snum = juju.nextDouble();
+	     answer = fnum + snum;
+	     System.out.println(answer);
        }
-       
 }
